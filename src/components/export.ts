b2b9fb107393eb6b3643999de557cf13bcf3fc82
Your Name <you@example.com>
2023-06-05 12:12:43 -0500

    export components all most 2

--- src/components/export.ts
+++ src/components/export.ts
@@ -1,6 +1,33 @@
-export {default as Categories } from './CategoriesButton/CategoriesButton'
-export {default as Tutorials } from './Tutorials/Tutorials'
+export {default as CategoriesButton } from './CategoriesButton/CategoriesButton'
+export {default as TutorialsCard } from './Tutorials/Tutorials'
 export {default as Psettings } from './ProfileSettings/ProfileSettings'
-export {default as Chats } from './Chats/Chats'
-export {default as Users } from './Users/Users'
 export { default as SeeTButton} from './SeeTButton/SeeTButton'
+export {default as AddNewVideoButton } from './AddNewVideoButton/AddNewVideoButton'
+export {default as BannerDashboard } from './BannerDashboard/BannerDashboard'
+export {default as BannerHome } from './BannerHome/BannerHome'
+export {default as ButtonInput } from './ButtonInput/ButtonInput'
+export {default as ButtonLogIn } from './ButtonLogIn/ButtonLogIn'
+export {default as ButtonLogInEnter } from './ButtonLoginEnter/ButtonLoginEnter'
+export {default as ButtonSignIn } from './ButtonSignIn/ButtonSignIn'
+export {default as ButtonSignInEnter } from './ButtonSignInEnter/ButtonSignInEnter'
+export {default as ConfigProfileDiv } from './ConfigProfileDiv/ConfigProfileDiv'
+export {default as CreateAButton } from './CreateAButton/CreateAButton'
+export {default as CreateTutorialButton } from './CreateTutorialButton/CreateTutorialButton'
+export {default as LogIntextDiv } from './LogintextDiv/LogintextDiv'
+export {default as LogoDiv } from './LogoDiv/LogoDiv'
+export {default as LogOutText } from './LogOutText/LogOutText'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
